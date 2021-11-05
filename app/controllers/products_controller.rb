0d7fa4840902d68ee8f667dbb71933a8a8871322
V. Bruk <vbruk@hey.com>
2021-11-05 11:12:57 -0300

    wip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :new]
 
   def index
     @products = Product.all
+  end
+
+  def new
+    # @product = Product.new
   end
 end
